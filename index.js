2d6a6ec2b2364770c8f0cff099af93498c99abcf
kimtaehyung <taehyeong77@naver.com>
2020-01-11 23:56:51 +0900

    commit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
 
 app.on('ready', () => {
-    const mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
-    // mainWindow.loadURL('google.com');
+});
+
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe(path, (err, metadata) => {
+        mainWindow.webContents.send('video:metadata', metadata.format.duration);
+    });
 });
